bb9ca1f89b41c5965baa2f7e42124c6298c10888
Daniel Proaño Chacón <danieluxury@gmail.com>
2023-03-24 15:47:18 -0500

    Use Generic View

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,13 +5,8 @@ from . import views
 app_name = 'polls'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    #ie: http://127.0.0.1:8000/polls/5/
-    #path('<int:question_id>/', views.detail, name ='detail'),
-    #ie: http://127.0.0.1:8000/polls/specifics/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    #ie: http://127.0.0.1:8000/polls/5/vote
-    path('<int:question_id>/vote', views.vote, name ='vote'),
-    #ie: http://127.0.0.1:8000/polls/5/results
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]

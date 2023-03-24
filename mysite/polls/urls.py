337c6685063bd266e85ae4045d99d4e6997d2b4a
Daniel Proaño Chacón <danieluxury@gmail.com>
2023-03-24 15:37:34 -0500

    Add results logic

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     #ie: http://127.0.0.1:8000/polls/5/
     #path('<int:question_id>/', views.detail, name ='detail'),
     #ie: http://127.0.0.1:8000/polls/specifics/5/
-    path('specifics/<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/', views.detail, name='detail'),
     #ie: http://127.0.0.1:8000/polls/5/vote
     path('<int:question_id>/vote', views.vote, name ='vote'),
     #ie: http://127.0.0.1:8000/polls/5/results
